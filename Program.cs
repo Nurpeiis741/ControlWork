eea3e26968dfe1716f2fe573e226fa35200ff0e5
sevensay <120299542+sevensay@users.noreply.github.com>
2023-02-16 17:37:33 +0600

    opisanie_1щ

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] NewStringArrayInput(int size) //задаем длину массива
+{
+    string[] newStringArray = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i}-й элемент массива: ");
+        newStringArray[i] = Console.ReadLine()!;
+        
+    }
+
+    return newStringArray;
+}
+
